338514b19182791952e22adf874c7b3ff29bba30
Thomas Schmitt <teschmitt@gmail.com>
2018-06-21 22:44:58 +0200

    Added accounts app and Profile Model

---
+++ ekozmp/apps/accounts/managers.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+# from .models import *
+
+
+class ProfileManager(models.Manager):
+    pass
--- ekozmp/apps/accounts/models.py
+++ ekozmp/apps/accounts/models.py
@@ -1,3 +1,97 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.utils.translation import ugettext_lazy as _
+from . import managers
+
+from ekozmp.apps.products.models import Product
+
+
+"""
+class MyModel(models.Model):
+    # Relations
+    # Attributes - Mandatory
+    # Attributes - Optional
+    # Object Manager
+    # Custom Properties
+    # Methods
+    # Meta and String
+"""
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                related_name="profile", verbose_name=_("user"),
+                                on_delete=models.CASCADE)
+
+    # Attributes - Mandatory
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    ACCOUNT_TYPES = (
+        ('S', _('Seller')),
+        ('B', _('Buyer')),
+        ('X', _('Seller and Buyer'))
+    )
+    account_type = models.CharField(max_length=1, choices=ACCOUNT_TYPES)
+    active = models.BooleanField(default=True)
+
+    # Attributes - Optional
+    language = models.CharField(max_length=10, blank=True)
+    timezone = models.CharField(max_length=32, blank=True)
+    """
+    Weitere Felder:
+    birthday
+    accepted_dse (Datenschutzerklärung)
+    accepted_cookiepolicy
+    pagination_prefs - items_per_page
+    """
+
+    # Object Manager
+    objects = managers.ProfileManager()
+
+    # Custom Properties
+    @property
+    def username(self):
+        return self.user.username
+
+    @property
+    def email(self):
+        return self.user.email
+
+    # Methods
+
+    # Meta and String
+    class Meta:
+        ordering = ("user",)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile_for_new_user(sender, created, instance, **kwargs):
+    if created:
+        profile = Profile(user=instance)
+        profile.save()
+
+
+class ProfileImage(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="ekozmp/static/img/profileimages/")
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return str(self.title)
+
+
+class ProfilePurchase(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __unicode__(self, ):
+        return self.profileprofile.username
